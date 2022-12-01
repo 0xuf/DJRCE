52c3dd215d807226c1f0ed42d9af920bb6ca64b7
0xuf <mobin.iwhh@gmail.com>
2022-12-02 02:58:41 +0330

    Create: Empty python file with banner function

---
+++ empty.py
@@ -0,0 +1,9 @@
+# return the banner
+def banner() -> str:
+    _banner = """
+|----------------------------------------------------|
+| Django rce example with leaked SECRET_KEY variable |
+|             github.com/0xuf/DJRCE.git              |
+|----------------------------------------------------|
+"""
+    return _banner
